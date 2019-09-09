03860c65a40629b1b4b4f943dc4e5b9b358d29ca
Marcio Correa <marciocorrea@Mac-mini-de-Marcio.local>
2019-09-09 18:20:32 -0300

    add reference

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open('README.md', 'r') as readme:
     README_TEXT = readme.read()
 
 setuptools.setup(
-    name='Parse-json2csv',
-    version='1.0',
+    name='parse_json2csv',
+    version='1.2',
     description='Easy-way to parser nested json <--> csv file.',
     long_description = README_TEXT,
     long_description_content_type='text/markdown',
